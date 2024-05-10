2c4e59eb8a9371561fd5b18cda12069c27e7f49f
John Hyla <jfhyla@gmail.com>
2024-05-10 16:51:36 -0400

    setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
 from setuptools import setup, find_packages
 
-from setuptools import setup, find_packages
-
 setup(
     name='ccl_segb',
     version='0.3.0',
+    url='https://github.com/snoop168/ccl-segb.git',
     packages=find_packages(),
 )
